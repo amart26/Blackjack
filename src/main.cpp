a9b06cb1bfe196548ee1d22b8e971d020a1d525d
amart26 <amartj0713@gmail.com>
2026-05-13 21:47:05 -0400

    Initial commit - Blackjack game with SDL3

---
+++ src/main.cpp
@@ -0,0 +1,300 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <SDL3/SDL.h>
+#include <SDL3_ttf/SDL_ttf.h>
+
+enum class GameState
+{
+    PLAYER_TURN,
+    DEALER_TURN,
+    GAME_OVER
+};
+
+enum class Suit
+{
+    HEARTS,
+    DIAMONDS,
+    CLUBS,
+    SPADES
+};
+
+enum class Rank
+{
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+};
+
+struct Card
+{
+    Suit suit;
+    Rank rank;
+};
+
+struct Deck
+{
+    std::vector<Card> cards;
+};
+
+struct Hand
+{
+    std::vector<Card> cards;
+};
+
+void fillDeck(Deck &deck)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 13; j++)
+        {
+            deck.cards.push_back(Card{static_cast<Suit>(i), static_cast<Rank>(j)});
+        }
+    }
+}
+
+void shuffleDeck(Deck &deck)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(deck.cards.begin(), deck.cards.end(), gen);
+}
+
+void dealCard(Deck &deck, Hand &hand)
+{
+    hand.cards.push_back(deck.cards.back());
+    deck.cards.pop_back();
+}
+
+int calculateScore(const Hand &hand)
+{
+    int sum = 0;
+    int aceCount = 0;
+    for (int i = 0; i < hand.cards.size(); i++)
+    {
+        switch (hand.cards[i].rank)
+        {
+        case Rank::JACK:
+            sum += 10;
+            break;
+        case Rank::QUEEN:
+            sum += 10;
+            break;
+        case Rank::KING:
+            sum += 10;
+            break;
+        case Rank::ACE:
+            aceCount++;
+            sum += 11;
+            break;
+        default:
+            sum += static_cast<int>(hand.cards[i].rank) + 1;
+            break;
+        }
+    }
+
+    while (sum > 21 && aceCount != 0)
+    {
+        aceCount--;
+        sum -= 10;
+    }
+    return sum;
+}
+
+int main()
+{
+    SDL_Init(SDL_INIT_VIDEO);
+    TTF_Init(); 
+    SDL_Log("SDL initialized successfully");
+    SDL_Window *window = SDL_CreateWindow("Blackjack", 800, 600, 0);
+    SDL_Log("Window created successfully");
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, nullptr);
+    SDL_SetRenderVSync(renderer, 1);
+    TTF_Font* font = TTF_OpenFont("assets/fonts/OpenSans.ttf", 24); 
+    if(font == nullptr)
+    {
+        SDL_Log("Font failed to laod: %s", SDL_GetError()); 
+    }
+
+    Deck deck;
+    Hand playerHand;
+    Hand dealerHand;
+
+    fillDeck(deck);
+    shuffleDeck(deck);
+    dealCard(deck, dealerHand);
+    dealCard(deck, dealerHand);
+
+    dealCard(deck, playerHand);
+    dealCard(deck, playerHand);
+
+    GameState gameState = GameState::PLAYER_TURN;
+    int playerScore = calculateScore(playerHand);
+    int dealerScore = calculateScore(dealerHand);
+
+    std::cout << "Players score: " << playerScore << "\n";
+    std::cout << "Dealers score " << dealerScore << "\n";
+    bool running = true;
+    bool isDealerDone = false;
+    bool isPlayerBusted = false;
+    bool isDealerBusted = false;
+    bool isGameOverHandled = false;
+    while (running)
+    {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+        if (event.type == SDL_EVENT_QUIT)
+        {
+            running = false;
+        }
+
+        if (event.type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+        {
+            float mouseX = event.button.x;
+            float mouseY = event.button.y;
+
+            if (mouseX >= 300 && mouseX <= 380 && mouseY >= 500 && mouseY <= 540)
+            {
+                std::cout << "HIT!\n";
+                dealCard(deck, playerHand);
+                playerScore = calculateScore(playerHand);
+                std::cout << playerScore << "\n";
+
+                if (playerScore > 21)
+                {
+                    isPlayerBusted = true;
+                    gameState = GameState::GAME_OVER;
+                    std::cout << "BUST! GAME OVER!\n";
+                }
+
+                if (playerScore == 21)
+                {
+                    gameState = GameState::GAME_OVER;
+                    std::cout << "BlACKJACK!\n";
+                }
+            }
+
+            if (mouseX >= 420 && mouseX <= 500 && mouseY >= 500 && mouseY <= 540)
+            {
+                std::cout << "STAND!, DEALERS TURN!\n";
+                gameState = GameState::DEALER_TURN;
+            }
+
+            if (gameState == GameState::GAME_OVER)
+            {
+
+                playerHand.cards.clear();
+                dealerHand.cards.clear();
+                deck.cards.clear();
+                fillDeck(deck);
+                shuffleDeck(deck);
+
+                dealCard(deck, playerHand);
+                dealCard(deck, dealerHand);
+
+                dealCard(deck, playerHand);
+                dealCard(deck, dealerHand);
+
+                playerScore = calculateScore(playerHand);
+                dealerScore = calculateScore(dealerHand);
+
+                isDealerDone = false;
+                isPlayerBusted = false;
+                isDealerBusted = false;
+                isGameOverHandled = false;
+
+                gameState = GameState::PLAYER_TURN;
+            }
+        }
+
+        SDL_SetRenderDrawColorFloat(renderer, 0.13f, 0.33f, 0.13f, 1.0f);
+        SDL_RenderClear(renderer);
+        SDL_FRect hitButton = {300, 500, 80, 40};
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &hitButton);
+        SDL_FRect standButton = {420, 500, 80, 40};
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+        SDL_RenderFillRect(renderer, &standButton);
+        if (gameState == GameState::GAME_OVER)
+        {
+            SDL_FRect playAgainButton = {360, 260, 80, 40};
+            SDL_SetRenderDrawColor(renderer, 250, 0, 0, 255);
+            SDL_RenderFillRect(renderer, &playAgainButton);
+        }
+        SDL_RenderPresent(renderer);
+
+        if (gameState == GameState::PLAYER_TURN)
+        {
+            // player logic will go here
+        }
+        else if (gameState == GameState::DEALER_TURN)
+        {
+            // dealer logic will go here
+            if (!isDealerDone)
+            {
+                dealerScore = calculateScore(dealerHand);
+                if (dealerScore > 21)
+                {
+                    isDealerBusted = true;
+                    gameState = GameState::GAME_OVER;
+                    std::cout << "GAME OVER! DEALER BUSTS!\n";
+                }
+                else if (dealerScore >= 17)
+                {
+                    isDealerDone = true;
+                    gameState = GameState::GAME_OVER;
+                    std::cout << dealerScore << "\n";
+                }
+                else if (dealerScore < 17)
+                {
+                    dealCard(deck, dealerHand);
+                    dealerScore = calculateScore(dealerHand);
+                }
+            }
+        }
+        else if (gameState == GameState::GAME_OVER)
+        {
+            if (!isGameOverHandled)
+            {
+                isGameOverHandled = true;
+                // show winner will go here
+                if (isPlayerBusted)
+                {
+                    std::cout << "DEALERS WIN - Player Busted\n";
+                }
+                else if (isDealerBusted)
+                {
+                    std::cout << "PLAYERS WIN - Dealers Busted\n";
+                }
+                else if (playerScore > dealerScore && !isPlayerBusted)
+                {
+                    std::cout << "PLAYER WINS\n";
+                }
+                else if (dealerScore > playerScore && !isDealerBusted)
+                {
+                    std::cout << "DEALER WINS\n";
+                }
+                else if (playerScore == dealerScore)
+                {
+                    std::cout << "PUSH!\n";
+                }
+            }
+        }
+    }
+
+    TTF_CloseFont(font); 
+    TTF_Quit(); 
+    SDL_Quit();
+}
